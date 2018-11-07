f0ab031cdc1b5beafcc747f7a3b918ede27bbffa
RAZMI <RAZMI@RAZMI-PC>
2018-11-07 20:10:56 +0530

    Initial check in

---
+++ rachu/src/main.c
@@ -0,0 +1,15 @@
+/*
+ * main.c
+ *
+ *  Created on: Nov 7, 2018
+ *      Author: RAZMI
+ */
+
+#include<stdio.h>
+int main()
+{
+	printf ("This pgm is for testing GIT version control");
+
+	return 0;
+}
+
