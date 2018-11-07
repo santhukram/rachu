4ce3246335660cc45a2a3e31392317d4915f09fc
RAZMI <RAZMI@RAZMI-PC>
2018-11-07 23:15:15 +0530

    Added for loop after step3

--- rachu/src/main.c
+++ rachu/src/main.c
@@ -8,10 +8,15 @@
 #include<stdio.h>
 int main()
 {
+	int i = 0;
 	printf ("This pgm is for testing GIT version control\n");
 	printf ("Step 1\n");
 	printf ("Step 2\n");
 	printf ("Step 3\n");
+	for (i = 0; i<=2; i++)
+		{
+		printf("%d\n",i);
+		}
 	return 0;
 }
 
